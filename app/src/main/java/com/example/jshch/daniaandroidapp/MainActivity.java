d494ccea3780c707a9dabbc4b7d6902f3b3a07e8
Jacob Saaby Hansen <jshchansen@gmail.com>
2018-03-19 12:20:18 +0100

    shake it up

--- app/src/main/java/com/example/jshch/daniaandroidapp/MainActivity.java
+++ app/src/main/java/com/example/jshch/daniaandroidapp/MainActivity.java
@@ -17,29 +17,46 @@ public class MainActivity extends AppCompatActivity {
     List<Sensor> listsensor;
     List<String> liststring ;
     ArrayAdapter<String> adapter ;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = (ListView)findViewById(R.id.listview1);
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
 
-        liststring = new ArrayList<String>();
+            @Override
+            public void onShake(int count) {
+				/*
+				 * The following method, "handleShakeEvent(count):" is a stub //
+				 * method you would use to setup whatever you want done once the
+				 * device has been shook.
+				 */
+                handleShakeEvent(count);
+            }
+        });
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
 
-        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+    private void handleShakeEvent(int count) {
 
-        listsensor = sensorManager.getSensorList(Sensor.TYPE_ALL);
-
-        for(int i=0; i<listsensor.size(); i++){
-
-            liststring.add(listsensor.get(i).getName());
-        }
-
-        adapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_2,
-                android.R.id.text1, liststring
-        );
-
-        listView.setAdapter(adapter);
     }
 }
